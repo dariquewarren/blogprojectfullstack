5a938c0d1f53fb3ab640ba668d11c564b2d31b68
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-11-23 23:51:08 -0500

    changed default image state value to undefined

--- blogproject/src/CreateArticle.js
+++ blogproject/src/CreateArticle.js
@@ -5,6 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
 import Dayjs from 'dayjs'
 import { db, writeUserData, addNewArticle, getArticlesByType} from './Firebase'
@@ -14,7 +15,7 @@ function CreateArticle(props) {
 const [title, setTitle] = useState('title')
 const [subtitle, setSubtitle] = useState('this is a test title')
 const [article, setArticle] = useState('this is an article that im writing, i hope its not frghtening. this is maybe lightning alright and yes okay.')
-const [image, setImage] = useState('blahal')
+const [image, setImage] = useState(undefined)
 const [imageURLInput, setImageURLInput] = useState(false)
 
 const [articleType, setArticleType] = useState('published')
@@ -42,48 +43,49 @@ const ImageRef = useRef()
             console.log('imageRef', ImageRef.current.files)
         }}
         >test button</Button>
-        <Form.Group className= 'mb-3'>
-        <img src={image} style={{height: '15rem', width: '15rem'}}/>
+      <Container>
+      <Form.Group className= 'mb-3'>
+      <img src={image} style={{height: '15rem', width: '15rem'}}/>
 <br/>
 <Form.Label>{(imageURLInput)?'Set Image URL':'Upload Image'} </Form.Label>
 {
-    (imageURLInput)
-        ?
-       <Row>
-       <Form.Control className='w-50' type='text' placeholder="Image URL"  onChange={(e)=>{
-        setImage(e.target.value)
-    }}/> <Button
-    className='w-50'
-    onClick={(e)=>{
-    e.preventDefault()
-    setImageURLInput(!imageURLInput)
-    }}
-    >{(imageURLInput)? 'Switch to Upload' : 'Switch to URL' }</Button>
-       </Row>
-        :
+  (imageURLInput)
+      ?
+     <Row>
+     <Form.Control className='w-50' type='text' placeholder="Image URL"  onChange={(e)=>{
+      setImage(e.target.value)
+  }}/> <Button
+  className='w-50'
+  onClick={(e)=>{
+  e.preventDefault()
+  setImageURLInput(!imageURLInput)
+  }}
+  >{(imageURLInput)? 'Switch to Upload' : 'Switch to URL' }</Button>
+     </Row>
+      :
 <Row>
 
 <Form.Control type='file' className='w-50' ref={ImageRef} onChange={(e)=>{
-    e.preventDefault()
-  
-    if(ImageRef.current){
-        let file = ImageRef.current.files[0]
-        console.log('current exists', file)
-        let fileReader = new FileReader(); 
+  e.preventDefault()
+
+  if(ImageRef.current){
+      let file = ImageRef.current.files[0]
+      console.log('current exists', file)
+      let fileReader = new FileReader(); 
 fileReader.readAsDataURL(file); 
 fileReader.onload = function() {
-  console.log('filereader result',fileReader.result);
-  setImage(fileReader.result)
+console.log('filereader result',fileReader.result);
+setImage(fileReader.result)
 }; 
 fileReader.onerror = function() {
- console.log('fileReader error',fileReader.error);
+console.log('fileReader error',fileReader.error);
 }; 
-        console.log('current exists', )
-    }else{
-        console.log('no current')
-        console.log('no current', ImageRef.current)
+      console.log('current exists', )
+  }else{
+      console.log('no current')
+      console.log('no current', ImageRef.current)
 
-    }
+  }
 }} />
 <Button
 className='w-50'
@@ -94,44 +96,49 @@ setImageURLInput(!imageURLInput)
 >{(imageURLInput)? 'Switch to Upload' : 'Switch to URL' }</Button>
 </Row>
 
-        } 
-       </Form.Group>
-        <Form 
-        onClick={(e)=>{
-            e.preventDefault()
-            console.log('article Submitted', newArticle)
-        }}
-        >
+      } 
+     </Form.Group>
+     
+     
+     
+     <Form 
+      onClick={(e)=>{
+          e.preventDefault()
+          console.log('article Submitted', newArticle)
+      }}
+      >
 <Row>
-        <Form.Group className= 'mb-3' as={Col}>
-        <Form.Label>Title</Form.Label>
-        <Form.Control type='text' placeholder="Title" value={title}  onChange={(e)=>{
-            setTitle(e.target.value)
-        }}/>
-        </Form.Group>
+      <Form.Group className= 'mb-3' as={Col}>
+      <Form.Label>Title</Form.Label>
+      <Form.Control type='text' placeholder="Title" value={title}  onChange={(e)=>{
+          setTitle(e.target.value)
+      }}/>
+      </Form.Group>
 
-        <Form.Group as={Col}>
-        <Form.Label>Sub-Title</Form.Label>
-        <Form.Control type='text' placeholder="Sub Title" onChange={(e)=>{
-            setSubtitle(e.target.value)
-        }}/>
-        </Form.Group>
+      <Form.Group as={Col}>
+      <Form.Label>Sub-Title</Form.Label>
+      <Form.Control type='text' placeholder="Sub Title" onChange={(e)=>{
+          setSubtitle(e.target.value)
+      }}/>
+      </Form.Group>
 
-        </Row>
+      </Row>
 
-       
+     
 
 
 
 
 <br/>Quill Editor
 <ReactQuill theme="snow" value={article}
-                  onChange={(e)=>{
-                      setArticle(e)
-                   console.log(article)
-                  }} />
-                  <Button type='submit'>Submit</Button>
-        </Form>
+                onChange={(e)=>{
+                    setArticle(e)
+                 console.log(article)
+                }} />
+                <Button type='submit'>Submit</Button>
+      </Form>
+      
+      </Container>
         </div>
     )
 }
