fadfd77ef3db4f0a84ad304039e52ac738035b98
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-11-24 12:30:05 -0500

    UI CHANGES

--- blogproject/src/Article.js
+++ blogproject/src/Article.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/esm/Row';
 
 function Article(props) {
+    var articleText= DOMParser.parseFromString(props.article)
     return (
         <Container>
         <h1 style={{textAlign:'center'}}>{props.title}</h1>
@@ -11,9 +12,11 @@ function Article(props) {
         <p className='w-50' style={{textAlign:'left'}}>By {props.author}</p><p className='w-50 ' style={{textAlign:'right'}}> {props.datePublished}</p>
        </Row>
         <br/>
-        <img src={props.image} style={{height: '20rem', width:'20rem'}}/>
-<br/>
-<h5>{props.article}</h5>
+        <Container style={{maxWidth:'60%', textAlign: 'right'}}>
+                <img src={props.image} style={{height: '20rem', width:'100%', display:'block', marginBottom:'2rem', marginLeft:'auto',marginRight:'0rem'}}/>
+                </Container>
+
+<h5>{articleText }</h5>
         </Container>
     )
 }
