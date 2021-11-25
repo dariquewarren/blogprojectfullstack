781738355ca4a86f5c0cdeae6ee4f856fd0e2edb
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-11-24 19:28:53 -0500

    refactored read article to look like read draft

--- blogproject/src/ReadArticle.js
+++ blogproject/src/ReadArticle.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from "react-router-dom";
+import Container from 'react-bootstrap/esm/Container'
+import Article from './Article'
+import { getSinglePublished } from './APICalls';
 
-function ReadArticle() {
+function ReadArticle(props) {
+
+    const objectArray = [{author: 'MR. Mister'}]
+ const {id} = useParams()
+
+ const [article, setArticle] = useState()
+    
+
+ useEffect(()=>{
+ if(!article){
+    getSinglePublished(id).then((data)=>{
+        return data.json()
+    }).then((data)=>{
+        if(!data.data || data.data.article === undefined ){
+            return  alert(data.message)  
+            }else{
+                console.log()
+             return setArticle(data.data)
+ 
+            }
+    })
+ }else {
+     return
+ }
+ }, [article])
+
+
     return (
-        <div>
-            
-        </div>
+        <Container>
+        <button
+        onClick={()=>{
+            console.log('article', article)     
+           }}
+        >
+        test button
+        </button>
+            <Article {...article} />
+        </Container>
     )
 }
-
 export default ReadArticle
