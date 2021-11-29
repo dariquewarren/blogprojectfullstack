7711d0101fb64a15b05d4e1b949c4d36b4138a25
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-11-29 14:06:02 -0500

    refactoredto include handlesignup function to handle...signup. added some state and console logs to check the data

--- blogproject/src/authComponents/SignupForm.js
+++ blogproject/src/authComponents/SignupForm.js
@@ -1,26 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 import Form from 'react-bootstrap/Form'
+import Button from '@restart/ui/esm/Button';
 function SignupForm() {
+const [userEmail, setUserEmail] = useState(undefined)
+const [userPassword, setUserPassword] = useState(undefined)
+
+
+const handleSignup = async (email, password)=>{
+    const auth = getAuth();
+await createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+    console.log('user created', user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+    console.log('errorMessage:',errorMessage, 'errorCode:', errorCode )
+  });
+}
+
     return (
         <div>
-    <Form>
+    <Form
+    onSubmit={(e)=>{
+        e.preventDefault()
+        handleSignup(userEmail, userPassword)
+    }}
+    >
+
     <Form.Group>
     <Form.Label>email</Form.Label>
-    <Form.Control type='email' />
+    <Form.Control type='email' 
+    onChange={(e)=>{
+        e.preventDefault()
+        setUserEmail(e.target.value)
+    }} />
     </Form.Group>
+
     <Form.Group>
     <Form.Label>password</Form.Label>
-    <Form.Control type='password' />
+    <Form.Control type='password' 
+    onChange={(e)=>{
+        e.preventDefault()
+        setUserPassword(e.target.value)
+    }} />
     </Form.Group>
+<Button type='submit'
+>Submit</Button>
     </Form>  
-    <Form.Group>
-    <Form.Label>first name</Form.Label>
-    <Form.Control type='text' />
-    </Form.Group>
-    <Form.Group>
-    <Form.Label>last name</Form.Label>
-    <Form.Control type='text' />
-    </Form.Group>
         </div>
     )
 }
