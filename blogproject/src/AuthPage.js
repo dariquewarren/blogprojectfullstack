0f34e0df530150cadfab4c279299faf6c17a0947
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-11-29 15:26:47 -0500

    switched Router to BrowserRouter for readability purposes(app.js). created a basic loginForm Component. imported it into the authpage component with the same props as signupform ie setUserInfo

--- blogproject/src/AuthPage.js
+++ blogproject/src/AuthPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import LoginForm from './authComponents/LoginForm'
 import SignupForm from './authComponents/SignupForm'
 
 function AuthPage(props) {
     return (
         <div>
-    <SignupForm userInfo={props.userInfo} setUserInfo={props.setUserInfo}/>        
+    <SignupForm userInfo={props.userInfo} setUserInfo={props.setUserInfo}/> 
+    <LoginForm userInfo={props.userInfo} setUserInfo={props.setUserInfo}/>       
         </div>
     )
 }
