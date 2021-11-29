92c1b437c42c3b0f07ce04d57f57a1c796364776
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-11-29 15:36:17 -0500

    added a space

--- blogproject/src/authComponents/LoginForm.js
+++ blogproject/src/authComponents/LoginForm.js
@@ -5,17 +5,42 @@ function LoginForm() {
     const [userEmail, setUserEmail] = useState(undefined)
     const [userPassword, setUserPassword] = useState(undefined)
 
+    const handleLogin =async (email, password)=>{
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+            console.log('user', user)
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log('errorCode',errorCode, 'errorMessage', errorMessage)
+          });
+    }
     return (
         <div>
         <h1>Sign Up</h1>
         <Form>
         <Form.Group>
         <Form.Label>email</Form.Label>
-        <Form.Control type='email'/>
+        <Form.Control required type='email' 
+        onChange={(e)=>{
+            e.preventDefault()
+            setUserEmail(e.target.value)
+        }}
+        />
         </Form.Group>
         <Form.Group>
         <Form.Label>password</Form.Label>
-        <Form.Control type='password' />
+        <Form.Control required type='password'
+        onChange={(e)=>{
+            e.preventDefault()
+            setUserPassword(e.target.value)
+        }}
+        />
         </Form.Group>
         </Form> 
         </div>
