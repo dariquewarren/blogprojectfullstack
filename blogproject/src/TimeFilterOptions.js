b7faec398c81b406ef8f52af58a54ce0d3355343
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-12-17 15:21:27 -0500

    refactored time filter to create a date to display

--- blogproject/src/TimeFilterOptions.js
+++ blogproject/src/TimeFilterOptions.js
@@ -1,22 +1,61 @@
 import React, {useState} from 'react'
+import dayjs from 'dayjs'
 
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 const TimeFilterOptions = (props)=>{
-    const [beginningTime, setBeginningTime] = useState(null)
-    const [endingTime, setEndingTime] = useState(null)
+const [beginningTime, setBeginningTime] = useState()
+const [endingTime, setEndingTime] =useState()
+
+const transformTime = (timeString)=>{
+  const timeNumber = (Number(timeString.replace(':', ''))) 
+
+  if(timeString[0] ==='0' && timeString[1]==='0' ){
+    let midnightString = `12:${timeNumber} AM`
+    console.log('midnight', midnightString)
+    return midnightString
+  }else if(timeNumber <= 1159 && timeNumber >= 1 ){
+    let morningString = `${timeString[0]}${timeString[1]}${timeString[2]}${timeString[3]}${timeString[4]} AM`
+    console.log('morning', morningString  )
+    return morningString
+  }else if(timeNumber < 1300 && timeNumber > 1159){
+  let noonString = `${timeString[0]}${timeString[1]}${timeString[2]}${timeString[3]}${timeString[4]} PM`
+  console.log('noon', timeNumber, noonString)
+  return noonString
+  }else if(timeNumber >= 1300){
+   let newTimeNumber = timeNumber - 1200
+    let afternoonTimeString =String(newTimeNumber)
+    let afternoonString =
+    (afternoonTimeString.length === 3)
+    ?
+    `${afternoonTimeString[0]}:${afternoonTimeString[1]}${afternoonTimeString[2]} PM`
+    :
+     `${afternoonTimeString[0]}${afternoonTimeString[1]}:${afternoonTimeString[2]}${afternoonTimeString[3]} PM`
+    console.log('afternoon', afternoonString)
+    return afternoonString
+  }else{
+    return '?????'
+  }
+ }
+
+let beginningTimeDisplay 
+let endingTimeDisplay
 
     const handleTimeFilter= ()=>{
  
         if(!beginningTime || !endingTime){
            alert(' both dates required to form a range')
        
+        }else if((Number(beginningTime.replace(':', ''))* 100) > (Number(endingTime.replace(':', '')) * 100) ){
+          alert(' beginning date must be later than ending date. ex: 12:00AM to 11:59PM')
+
         }else{
        
-           const beginningTimeNumber = (Number(beginningTime.replace(':', ''))* 100) 
-               const endingTimeNumber = (Number(endingTime.replace(':', '')) * 100)
-               
+          
+          const beginningTimeNumber = (Number(beginningTime.replace(':', ''))* 100) 
+          const endingTimeNumber = (Number(endingTime.replace(':', '')) * 100)
+        
            const filteredTimeArray = props.mappedArray.filter((f)=>{
                
                return f.sortableTime >= beginningTimeNumber && f.sortableTime <= endingTimeNumber 
@@ -30,7 +69,13 @@ const TimeFilterOptions = (props)=>{
            return  alert('no data in range')
        
            }else{
-            return props.closeAllOptions()
+             
+          beginningTimeDisplay = transformTime (beginningTime)
+          endingTimeDisplay = transformTime (endingTime)            
+             
+             props.setFilterMessage(`Showing messages from ${beginningTimeDisplay} to ${endingTimeDisplay}`)
+            props.closeAllOptions()
+            return props.setMappedArray(filteredTimeArray)
        
            }
         }
@@ -40,7 +85,7 @@ const TimeFilterOptions = (props)=>{
     return(
         <Row>
         <Form.Group style={{width:'50%'}}>
-        <Form.Label style={{width:'100%'}}>start Time {beginningTime}</Form.Label>
+        <Form.Label style={{width:'100%'}}>start Time {beginningTimeDisplay}</Form.Label>
         <Form.Control 
         style={{width:'100%'}}
         type='time'                  
@@ -52,7 +97,7 @@ const TimeFilterOptions = (props)=>{
         
         </Form.Group>
         <Form.Group style={{width:'50%'}}>
-        <Form.Label style={{width:'100%'}}>end Time {endingTime}</Form.Label>
+        <Form.Label style={{width:'100%'}}>end Time {endingTimeDisplay}</Form.Label>
         <Form.Control 
         style={{width:'100%'}}
         type='time' 
