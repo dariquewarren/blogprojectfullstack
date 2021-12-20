327a432075cd33d607a440886d492fd31f769c1a
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-12-20 15:28:46 -0500

    made ReadPublished identical to readDrafts

--- blogproject/src/ReadPublished.js
+++ blogproject/src/ReadPublished.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from "react-router-dom";
+import Container from 'react-bootstrap/esm/Container'
+import Article from './Article'
+import { getSingleDraft } from './APICalls';
+import Loading from './Loading';
+function ReadPublished(props) {
 
-function ReadPublished() {
+    const objectArray = [{author: 'MR. Mister'}]
+ const {id} = useParams()
+
+ const [draft, setDraft] = useState()
+    
+
+ useEffect(()=>{
+ if(!draft){
+    getSingleDraft(id).then((data)=>{
+        return data.json()
+    }).then((data)=>{
+        if(!data.data || data.data.article === undefined ){
+            return  alert(data.message)  
+            }else{
+                console.log()
+             return setDraft(data.data)
+ 
+            }
+    })
+ }else {
+     return
+ }
+ }, [draft])
+
+
     return (
-        <div>
-            
-        </div>
+        <Container>
+        {(draft)?
+    <Article {...draft} />
+        :
+    <Loading/>
+        }
+        </Container>
     )
 }
 
