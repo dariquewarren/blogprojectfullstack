37c327bc5e75b64bbc69a31a9c844835192c6b2e
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-12-27 09:27:29 -0500

    switched root route to lead to homepage

--- blogproject/src/App.js
+++ blogproject/src/App.js
@@ -85,7 +85,7 @@ const testArticle = {
    {(userInfo)?userInfo.email : 'undefined'   }
     <Switch>
 
-    <Route exact path='/' element={<ViewDrafts articleAuthor={articleAuthor}/>}/>
+    <Route exact path='/' element={<Homepage articleArray={dummyArticleArray} trueArray={trueArray} handleRetrieveData={handleRetrieveData} author={articleAuthor} />}/>
 
     <Route exact path='/write' element={<CreateArticle articleAuthor={articleAuthor} />}/>
     
