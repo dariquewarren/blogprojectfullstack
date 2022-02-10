7419dfc25bc2a9551ca6dadfb6113e97b09f6a21
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2022-02-09 19:27:38 -0500

    ui changes

--- blogproject/src/Article.js
+++ blogproject/src/Article.js
@@ -4,34 +4,66 @@ import Container from 'react-bootstrap/Container';
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 function Article(props) {
-
+const tagsWidth = (100 / props.tags.length) - 8
    
 
     useEffect(()=>{
         
        },[props])
     return (
-       <Container>
+       <Container style={{display:'flex',flexDirection:'row' }}>
        
       
-        <div style={{marginBottom:'2rem'}}>
+        <div style={{marginBottom:'3rem', width:'70%', marginLeft:'2%', marginRight:'2.5%'}}>
         <h1 className='text-center mt-2rem mb-2rem'>{props.title}</h1>
-<h4 style={{marginBottom:'1rem',marginTop:'1rem'}}>{props.subtitle}</h4>
-<h6 style={{marginBottom:'1rem',marginTop:'1rem'}}>By {props.author}</h6>
+<h5 style={{textAlign:'center',marginBottom:'1rem',marginTop:'1rem', fontStyle:'italic'}}>{props.subtitle}</h5>
+<p style={{marginBottom:'1rem',marginTop:'1rem'}}>By {props.author}</p>
 
-<div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems:'center',marginBottom:'2rem',marginTop:'2rem'}}>
+<div style={{borderBottom:'3px solid black' ,display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems:'center',marginBottom:'2rem',marginTop:'2rem'}}>
 <p  style={{width:'50%', textAlign:'left'}}>{props.datePublished}</p>
 <p style={{width:'50%', textAlign:'right'}}>{props.timePublished}</p>
 </div>
+
+
 <div style={{marginBottom:'2rem',marginTop:'2rem'}}>
 <img src={props.image} style={{height: '25rem', width:'80%', marginLeft:'10%', marginRight:'10%'}}/>
 
 </div>
-{ReactHtmlParser(props.article)}
+
+{ReactHtmlParser(props.article)} 
+
 </div>
          
-        
-       
+
+<div style={{ height:'500px', borderTop: '2px solid black',borderBottom: '2px solid black',width: '25%', marginLeft:'2.5%', marginRight:'1px'}}>
+<h4 style={{textAlign:'center'}}>Tags: </h4>
+<hr/>
+{(props.tags)? props.tags.map((m)=>{
+   return(
+      <p key={m}
+      style={{width:`${tagsWidth}%`, marginLeft:'auto', marginRight:'auto', textAlign:'right'}}
+      >{m}</p>
+   )
+})
+:
+<p></p>
+}
+<br/>
+<h4 style={{textAlign:'center'}}>Categories: </h4>
+<hr/>
+{(props.category)? props.category.map((m)=>{
+   return(
+      <p key={m}
+      style={{width:`${tagsWidth}%`, marginLeft:'auto', marginRight:'auto', textAlign:'center'}}
+      >{m}</p>
+   )
+})
+:
+<p></p>
+}
+
+</div>
+
        </Container>
     )
 }
--- blogproject/src/Footer.js
+++ blogproject/src/Footer.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 function Footer() {
     return (
-        <div style={{border: '4px solid #212121', textAlign: 'center', height: '20vw'}}>
+        <div style={{border: '4px solid #212121', textAlign: 'center', height: '15rem'}}>
             
-        <Container fluid style={{border:'2px solid red',height: '10vw', marginTop: '5vw', marginBottom: '5vw'}}> 
+        <Container fluid style={{borderTop:'2px dashed red',height: 'auto', marginTop: 'auto', marginBottom: 'auto'}}> 
         <h3> Code by: Darique Warren</h3>
-        <h3> Lifestyle: Creator</h3>
-        <h3> POV: *See Lifestyle* {window.location.pathname}</h3>
+        <h3> EvilBunny Enterprises</h3>
+        <h3> Phone: (123) 456-7890</h3>
+        <h3> Email: BunnyMGMT@bunny.com</h3>
+
 
         </Container>
         </div>
